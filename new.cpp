d71f3a192e9f750471c4845e5d6470bc5fef98ef
RevanthChetluru <c.reva1996@gmail.com>
2016-11-03 10:45:31 +0530

    Added getch() to wait for keystroke before closing

--- new.cpp
+++ new.cpp
@@ -1,16 +1,18 @@
-#include <iostream>      																	//For I/O Functions
-#include <windows.h>   																		//For Beep & Sleep functions
-#include <stdlib.h>  																		//For System Pause
+#include <iostream>      																	// For I/O Functions
+#include <windows.h>   																		// For Beep & Sleep functions
+#include <stdlib.h>  																		// For System Pause
+#include <conio.h>                                                                          // For getch() at the end
 
 using namespace std;
 
-int typeWait = 50; 																			//Number of milliseconds to wait before typing next charactar.
+int typeWait = 50; 																			// Number of milliseconds to wait before typing next charactar.
 
-/**	Input: 
- *	@lpParameter - Contains the string. 
+/**	
+ *  Input: 
+ *  @lpParameter - Contains the string. 
  *
- *	Prints each character of a string with 
- *	a gap of 50 milliseconds. 
+ *  Prints each character of a string with 
+ *  a gap of 50 milliseconds. 
  */
 DWORD WINAPI typingThread(LPVOID lpParameter)
 {
@@ -25,14 +27,15 @@ DWORD WINAPI typingThread(LPVOID lpParameter)
         return 0;
 }
 
-/**	Input: 
- *	@s - The string to be typed.
+/**	
+ *  Input: 
+ *  @s - The string to be typed.
  *  @h - The HANDLE of the previous typeString thread.
  *
- *	Waits for the previous typeString thread to finish executing
+ *  Waits for the previous typeString thread to finish executing
  *  and creates a thread to type string s. 
  */
-HANDLE typeMessage(string msg, HANDLE h)
+HANDLE typeMessage(string &msg, HANDLE h)
 {
     if(h!=0)
         WaitForSingleObject(h, INFINITE); 													// Wait till the previous thread of typeString completes execution.
@@ -41,27 +44,24 @@ HANDLE typeMessage(string msg, HANDLE h)
     return myHandle;
 }
 
-/**
- * TODO:
- * Add getch() before closing the program. 
- */
 int main()
 {
 
-    /**				Frequency Chart
-    Octave	1  2  3   4   5   6    7    8
-    C 		16 33 65 131 262 523 1046 2093
-    C# 		17 35 69 139 277 554 1109 2217
-    D 		18 37 73 147 294 587 1175 2349
-    D# 		19 39 78 155 311 622 1244 2489
-    E 		21 41 82 165 330 659 1328 2637
-    F 		22 44 87 175 349 698 1397 2794
-    F# 		23 46 92 185 370 740 1480 2960
-    G 		24 49 98 196 392 784 1568 3136
-    G# 		26 52 104 208 415 831 1661 3322
-    A 		27 55 110 220 440 880 1760 3520
-    A# 		29 58 116 233 466 932 1865 3729
-    B 		31 62 123 245 494 988 1975 3951
+    /**	
+    *			Frequency Chart
+    *   Octave	1  2  3   4   5   6    7    8
+    *   C 		16 33 65 131 262 523 1046 2093
+    *   C# 		17 35 69 139 277 554 1109 2217
+    *   D 		18 37 73 147 294 587 1175 2349
+    *   D# 		19 39 78 155 311 622 1244 2489
+    *   E 		21 41 82 165 330 659 1328 2637
+    *   F 		22 44 87 175 349 698 1397 2794
+    *   F# 		23 46 92 185 370 740 1480 2960
+    *   G 		24 49 98 196 392 784 1568 3136
+    *   G# 		26 52 104 208 415 831 1661 3322
+    *   A 		27 55 110 220 440 880 1760 3520
+    *   A# 		29 58 116 233 466 932 1865 3729
+    *   B 		31 62 123 245 494 988 1975 3951
     **/
 
     int C = 523;
@@ -91,7 +91,7 @@ int main()
 
 
     s = "Happy Birthday to you...\n";
-    h = typeMessage(s,h);
+    h = typeMessage(s, h);
 
     //Tune: CCCGF
     Beep(C, 282);
@@ -124,8 +124,10 @@ int main()
     Beep(G, 286);
     Beep(F, 270);
 
-    s = "";
+    s = "\nPress any key to exit...";
     h = typeMessage(s, h);
+
+    getch();
 
     return 0;
 }
